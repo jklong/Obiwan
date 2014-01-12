d3ef9183961ed4d7375918e501c755cf067eba23
james <james@galactica.(none)>
2014-01-12 22:30:34 +1100

    Initial commit, yoda.py

---
+++ yoda.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit('Usage: yoda.py [video file]')
+
+cap = cv2.VideoCapture(sys.argv[1])
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', gray )
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
