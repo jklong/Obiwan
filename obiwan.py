72a7c8a53529bed24b52f5a83dcd560923fe39fe
james <james@galactica.(none)>
2014-01-18 17:19:27 +1100

    Add calcuation of velocity and acceleration.
    NOTE: Magnitude of force (i.e. acceleration from calc_accel) is inverted
    in sign, should be positive.
    TODO: Translate video coordinates to cartesian. Should fix above.
    TODO: Refactor to remove class

--- obiwan.py
+++ obiwan.py
@@ -2,34 +2,68 @@ import numpy as np
 import cv2
 import sys
 import math
+import matplotlib.pyplot as plt
 
 class Obiwan:
+
+    def get_roi_corners(self):
+        
+        roi_x1 = self.roi_x - self.roi_size
+        roi_y1 = self.roi_y - self.roi_size
+        roi_x2 = self.roi_x + self.roi_size
+        roi_y2 = self.roi_y + self.roi_size
+        
+        return (roi_x1, roi_y1, roi_x2, roi_y2)
 
     def set_roi(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONUP:
             self.roi_x = x
             self.roi_y = y
-            roi_x1 = self.roi_x - self.roi_size
-            roi_y1 = self.roi_y - self.roi_size
-            roi_x2 = self.roi_x + self.roi_size
-            roi_y2 = self.roi_y + self.roi_size
-            self.roi = self.img[roi_y1:roi_y2, roi_x1:roi_x2]
+
+            x1,y1,x2,y2 = self.get_roi_corners()
+
+            self.roi = self.img[y1:y2, x1:x2]
             print '#ROI set at t= '+ str(self.t/self.vidFPS)
 
     def set_roi_size(self, r):
         self.roi_size = r
 
     def draw_roi(self):
-        roi_x1 = self.roi_x - self.roi_size
-        roi_y1 = self.roi_y - self.roi_size
-        roi_x2 = self.roi_x + self.roi_size
-        roi_y2 = self.roi_y + self.roi_size
+        x1,y1,x2,y2 = self.get_roi_corners()
 
-        cv2.rectangle(self.img, (roi_x1, roi_y1), (roi_x2, roi_y2), (0, 0, 255), 1)
+        cv2.rectangle(self.img, (x1, y1), (x2, y2), (0, 0, 255), 1)
         cv2.circle(self.img, (self.roi_x, self.roi_y), 2, (255, 0, 0), 1)
 
-        if self.roi is None:
-            self.roi = self.img[roi_y1:roi_y2, roi_x1:roi_x2]
+    def calc_vel(self, pts):
+        #Returns vector of pixel velocity between frames
+        #Assume stationary from beginning of film
+        vel = [0.0]
+        
+        # We want magnitude of the velocity only, we don't care about direction
+        # v = dr/dt. Assume cartesian distance, dt = 1 (frame)
+        for i in range(1, len(pts)-1):
+          x0,y0 = pts[i-1]
+          x1,y1 = pts[i]
+          
+          r = math.sqrt((y1-y0)**2 + (x1-x0)**2)
+          vel.append(r)
+
+        return vel
+
+    def calc_accel(self,vel):
+        accel = []
+
+
+        # Magnitude of acceleration only, we don't have the direction anyway
+        # a = dv/dt, dt = 1 (frame)
+        for i in range(1, len(vel)):
+            u = vel[i-1]
+            v = vel[1]
+
+            a = v-u
+            accel.append(a)
+
+        return accel
 
     def __init__(self, argv):
         
@@ -63,6 +97,9 @@ class Obiwan:
 
     def go(self):
         
+        roi_points = []
+
+
         ret, self.img = self.cap.read()
         self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         cv2.imshow('Obiwan',self.img)
@@ -76,24 +113,33 @@ class Obiwan:
         while(1):
             self.t += 1
             ret, self.img = self.cap.read()
-            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
 
             if self.img is None:
                 break
 
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
             res = cv2.matchTemplate(self.img, self.roi, cv2.TM_CCOEFF_NORMED)
             min_v, max_v, min_loc, max_loc = cv2.minMaxLoc(res)
            
-            self.roi_x, self.roi_y  = max_loc
+            self.roi_x, self.roi_y = max_loc
             self.roi_x += self.roi_size 
             self.roi_y += self.roi_size
-            print str(self.t) + ',' + str(self.roi_x) + ',' + str(self.roi_y)
+            roi_points.append((self.roi_x, self.roi_y))
 
             self.draw_roi()
             cv2.imshow('Obiwan',self.img)
                  
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
+        if len(roi_points) > 2:
+            roi_vel = self.calc_vel(roi_points)
+            accs = self.calc_accel(roi_vel)
+            plt.plot(accs)
+            plt.ylabel('Force')
+            plt.show()
+            print max(accs)
+
 
         self.cap.release()
         cv2.destroyAllWindows()
