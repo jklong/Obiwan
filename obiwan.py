858463f9ac1d9ff2b4ef803b8d85bb817b3bbf67
james <james@galactica.(none)>
2014-01-22 19:17:03 +1100

    Add graphing of velocity in blue. Combined with vertical displacement only it gives a sensible graph.

--- obiwan.py
+++ obiwan.py
@@ -37,7 +37,7 @@ def draw_roi():
     cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
     cv2.circle(img, (roi_x, roi_y), 2, (255, 0, 0), 1)
 
-def convert_screen_to_cartesian(pts):
+def convScreenToCart(pts):
     
     #Screen coordinates have (0,0) at top left but cartesian has bottom left
 
@@ -106,7 +106,7 @@ cv2.createTrackbar('Start', 'obwControls', 0, int(vidLen), setStartFrame )
 cv2.createTrackbar('End','obwControls',int(vidLen),int(vidLen), setEndFrame )
 
 roi_points = []
-vel = [0.0]
+vel = []
 acc = []
 seconds = []
 #if true the graph will display and update in real time. Slows down execution
@@ -124,6 +124,14 @@ while (roi_x == -1 or roi_y == -1):
 if getVidPosition(cap) != startFrame:
     #If the last frame we saw before starting was an end frame, reset to start point
     set_vid_position(cap,startFrame)
+    ret, img = cap.read()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    res = cv2.matchTemplate(img, roi, cv2.TM_CCOEFF_NORMED)
+    min_v, max_v, min_loc, max_loc = cv2.minMaxLoc(res)
+   
+    roi_x, roi_y = max_loc
+    roi_x += roi_size 
+    roi_y += roi_size
 
 roi_points.append((roi_x,roi_y))
 draw_roi()
@@ -148,29 +156,34 @@ while(1):
     x0,y0 = roi_points[-2]
     x1,y1 = roi_points[-1]
     
-    vel.append(math.sqrt((roi_y-y0)**2 + (roi_x-x0)**2))
+    #vel.append(math.sqrt((roi_y-y0)**2 + (roi_x-x0)**2))
     #Only taking vertical component - test
-    #vel.append(y1-y0)
+    vel.append(y1-y0)
 
     #Calculate acceleration
-    acc.append(vel[-1] - vel[-2])
+    if len(vel) == 1:
+        acc.append(vel[-1])
+    else:
+        acc.append(vel[-1] - vel[-2])
 
     seconds.append(getVidPosition(cap)/vidFPS) 
 
     draw_roi()
+
     cv2.putText(img, str(round(seconds[-1],3)), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1,255 )
+
     cv2.imshow('Obiwan',img)
     if ANIMATE_GRAPH and getVidPosition(cap) % 30 == 0:
-        plt.plot(seconds, acc, 'r-', linewidth=1)
+        plt.plot(seconds, acc, 'r-', seconds, vel, 'b-', linewidth=1)
         plt.ylabel('Force')
         plt.draw()
          
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+    
 cap.release()
 cv2.destroyAllWindows()
-plt.plot(seconds, acc, 'r-', linewidth=1)
+plt.plot(seconds, acc, 'r-', seconds, vel, 'b-', linewidth=1)
 plt.ylabel('Force')
 plt.ioff()
 plt.show()
